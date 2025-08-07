ebbe01c542386f7b26ed69b343d7506ef10bb3a3
Samuel Torres <samuelvianatorres@gmail.com>
2025-08-07 20:11:19 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFixacao3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp
+{
+    internal class ExercicioFixacao3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a nota do primeiro semestre: ");
+            double nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite a nota do segundo semestre: ");
+            double nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double resultado = nota1 + nota2;
+
+            if (resultado < 60.00) 
+            {
+                Console.WriteLine("Nota Final " + resultado.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reprovado");
+            }
+            else 
+            {
+                Console.WriteLine("Nota Final " + resultado.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aprovado");
+            }
+
+        }
+    }
+}
